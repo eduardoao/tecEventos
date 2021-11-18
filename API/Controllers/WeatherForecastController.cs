3f3b7e9af08d7f48de02999c9face8612ce1a3e1
Eduardo Alcantara de Oliveira <eoliveira@trt4.jus.br>
2021-11-18 17:34:35 -0300

    Test:Update by CD

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace tecEvento.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Fucking", "Bracing", "Chilly", "FDP", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
